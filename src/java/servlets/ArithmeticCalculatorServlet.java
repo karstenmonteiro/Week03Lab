bb086aad4d37067908274d49b3ab367c39dfdec5
karstenmonteiro <karstenmonteiro@kmLaptop>
2023-01-23 19:50:47 -0700

    Week 3 Lab: Calculators (completed) Added part 2, Arithmetic Calculator. This calculator can perform addition, subtraction, multiplication, or modulos operations when given two numbers, entered by the user. This is the completed version of the week 3 lab, Calculators.

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -35,6 +35,53 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // get the parameters from the "arithmeticcalculator.jsp" form
+        String first = request.getParameter("first");
+        String second = request.getParameter("second");
+        String add = request.getParameter("add");
+        String subtract = request.getParameter("subtract");
+        String multiply = request.getParameter("multiply");
+        String modulos = request.getParameter("modulos");
+        int result;
         
+        // keep the text boxes populated with the values the user previously entered
+        request.setAttribute("first", first);
+        request.setAttribute("second", second);
+        
+        /** validation */
+        if (first == null || first.equals("") || second == null || second.equals("") || (!first.matches("[0-9]+")) || (!second.matches("[0-9]+"))) {
+            // null and/or invalid value(s), display error message
+            request.setAttribute("resultMsg", "invalid");
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+        }
+        /** submit button actions */
+        else if (add != null) {
+            // calculate result using addition
+            result = Integer.parseInt(first) + Integer.parseInt(second);
+            // display result message
+            request.setAttribute("resultMsg", result);
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+        }
+        else if (subtract != null) {
+            // calculate result using subtraction
+            result = Integer.parseInt(first) - Integer.parseInt(second);
+            // display result message
+            request.setAttribute("resultMsg", result);
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+        }
+        else if (multiply != null) {
+            // calculate result using multiplication
+            result = Integer.parseInt(first) * Integer.parseInt(second);
+            // display result message
+            request.setAttribute("resultMsg", result);
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+        }
+        else if (modulos != null) {
+            // calculate result using modulos
+            result = Integer.parseInt(first) % Integer.parseInt(second);
+            // display result message
+            request.setAttribute("resultMsg", result);
+            getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
+        }
     }
 }
